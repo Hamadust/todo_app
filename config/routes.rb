628c80373261ee6f5d5397d2489aeede5e8527b5
Hamadust <wake.awakens@gmail.com>
2021-07-04 16:29:54 +0900

    Finished Modeling-User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'messages#index'
+  get  'signup', to: 'users#new'
   resources :messages
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_111229) do
+ActiveRecord::Schema.define(version: 2021_07_04_065258) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
@@ -19,9 +19,12 @@ ActiveRecord::Schema.define(version: 2021_07_01_111229) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "new"
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
