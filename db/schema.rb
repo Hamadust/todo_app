e43beb3f1d7d48c6d8ec334d15ab3c10a52fe042
Hamadust <wake.awakens@gmail.com>
2021-07-04 18:49:57 +0900

    Finished Advanced-Login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_065258) do
+ActiveRecord::Schema.define(version: 2021_07_04_094838) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_065258) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
