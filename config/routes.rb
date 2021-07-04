ae29e4d72a1482058dc654ed21847435bbc1504b
Hamadust <wake.awakens@gmail.com>
2021-07-04 18:47:51 +0900

    Finished Basic-Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'messages#index'
-  get  'signup', to: 'users#new'
+  root   'messages#index'
+  get    'signup',   to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :messages
   resources :users
 end
