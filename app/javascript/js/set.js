1dfc35c3a43b31f5abf8fdabec4f2fec1a3dada5
Hamadust <wake.awakens@gmail.com>
2021-07-10 22:31:17 +0900

    フォームのステータスのスタイルを変更

--- app/javascript/js/set.js
+++ app/javascript/js/set.js
@@ -1,9 +1,9 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   // 「値をセット」ボタンをクリックしたらテキストボックスにログインに必要な値がセットされる
-//   document.getElementById('sampleUserSet').addEventListener('click', function () {
-//     var email = document.getElementById('session_email')
-//     var password = document.getElementById('session_password')
-//     email.value = 'sample-user@example.com'
-//     password.value = 'sample-user'
-//   })
-// }, false);
+document.addEventListener("DOMContentLoaded", function () {
+  // 「値をセット」ボタンをクリックしたらテキストボックスにログインに必要な値がセットされる
+  document.getElementById('sampleUserSet').addEventListener('click', function () {
+    var email = document.getElementById('session_email')
+    var password = document.getElementById('session_password')
+    email.value = 'sample-user@example.com'
+    password.value = 'sample-user'
+  })
+}, false);
